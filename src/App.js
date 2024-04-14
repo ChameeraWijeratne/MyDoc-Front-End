9110c417838b8b8dea86152f7162f9fad2d1408d
RashmiTharushika <rashmitharushika.info@gmailcom>
2024-04-15 00:17:18 +0530

    'Add-login-page'

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-
+import Login from './pages/Login/Login';
 import Home from './pages/Home/Home';
 import Register from './pages/Signup/Signup';
 import DoctorReg from './pages/DoctorReg/DoctorReg';
@@ -11,16 +11,18 @@ import Approvals from './pages/Approvals/Approvals';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<Register />} />
-        <Route path="/userreg" element={<UserReg />} />
-        <Route path="/docreg" element={<DoctorReg />} />
-        <Route path="/doctors" element={<Doctors />} />
-        <Route path="/approvals" element={<Approvals />} />
-      </Routes>
-    </Router>
+    // <Router>
+    //   <Routes>
+    //     <Route path="/" element={<Home />} />
+    //     <Route path="/signup" element={<Register />} />
+    //     <Route path="/userreg" element={<UserReg />} />
+    //     <Route path="/docreg" element={<DoctorReg />} />
+    //     <Route path="/doctors" element={<Doctors />} />
+    //     <Route path="/approvals" element={<Approvals />} />
+    //   </Routes>
+    // </Router>
+
+<Login/>
   );
 }
 
